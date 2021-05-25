031dd1c14928ca84af8b2a2cb45f1fe18a1826d5
joonion <joonion@gmail.com>
2021-05-25 09:27:18 +0900

    Chapter 8

--- Chap.08/8.1_FindingBoth.1.py
+++ Chap.08/8.1_FindingBoth.1.py
@@ -1,17 +1,3 @@
-def find_largest(n, S):
-    largest = S[0]
-    for i in range(n):
-        if largest < S[i]:
-            largest = S[i]
-    return largest
-
-def find_smallest(n, S):
-    smallest = S[0]
-    for i in range(n):
-        if smallest > S[i]:
-            smallest = S[i]
-    return smallest
-
 def find_both(n, S):
     largest, smallest = S[0], S[0]
     for i in range(n):
--- Chap.08/8.2_FindingBoth.2.py
+++ Chap.08/8.2_FindingBoth.2.py
@@ -1,3 +1,17 @@
+def find_largest(n, S):
+    largest = S[0]
+    for i in range(n):
+        if largest < S[i]:
+            largest = S[i]
+    return largest
+
+def find_smallest(n, S):
+    smallest = S[0]
+    for i in range(n):
+        if smallest > S[i]:
+            smallest = S[i]
+    return smallest
+    
 def find_both(n, S):
     winner, loser = [], []
     for i in range(0, n, 2):
